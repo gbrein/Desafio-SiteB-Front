8f332a50178bd6a895e6e603ca50109ebc98efe1
gbrein <gbrein@gmail.com>
2019-06-21 15:58:52 -0300

    Form e estados funcionando

--- src/App.js
+++ src/App.js
@@ -1,20 +1,54 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import {Switch, Route} from 'react-router-dom';
 import NavB from './Components/Nav'
 import ListaDominios from './Components/ListaDominios'
 import Home from './Components/Home'
 
-function App() {
-    return (
-        <div className="App">
-            <NavB/>
-            <Switch>
-                <Route exact path='/' component={Home}/>
-                <Route path='/ListaDominios' component={ListaDominios}/>
-            </Switch>
-        </div>
-    );
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            listaDominios: '',
+            baseForm: {
+                nome: '',
+                endereco: '',
+                cpf: '',
+                email: '',
+                telefone: '',
+                dominios: ''
+            }
+        }
+        this.handleUpdate = this
+            .handleUpdate
+            .bind(this);
+    }
+
+    handleUpdate(value, id) {
+        let newBaseForm = this.state.baseForm
+        if (newBaseForm[id] === undefined) {
+            newBaseForm[id] = value
+        } else {
+            newBaseForm[id] += value;
+        }
+       return this.setState({baseForm: newBaseForm})
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <NavB/>
+                <Switch>
+                    <Route
+                        exact
+                        path='/'
+                        render=
+                        { ()=> <Home action={this.handleUpdate} baseForm={this.state.baseForm} />}/>
+                    <Route path='/ListaDominios' component={ListaDominios}/>
+                </Switch>
+            </div>
+        )
+    }
 }
 
 export default App;
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,12 +1,91 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
-import FormClient from './FormClient'
+import {Form, Button, Col} from 'react-bootstrap';
 
 class Home extends Component {
+    constructor(props) {
+        super(props);
+        this.handleOnChange = this
+            .handleOnChange
+            .bind(this);
+    }
+
+    handleOnChange(event) {
+        const {action} = this.props;
+        const value = event.target.value;
+        const id = event.target.id;
+        action(value, id)
+    }
+
     render() {
+        const {baseForm} = this.props;
         return (
-            <div>
-                <FormClient/>
+            <div className="divForm">
+                <Form onSubmit= {e => this.handleSubmit(e)}>
+
+                    <Form.Row>
+                        <Form.Group as={Col} controlId="nome">
+                            <Form.Label>Nome Completo</Form.Label>
+                            <Form.Control defaultValue={baseForm.nome} onChange={this.handleOnChange} required/>
+                        </Form.Group>
+                    </Form.Row>
+
+                    <Form.Row>
+                        <Form.Group as={Col} controlId="endereco">
+                            <Form.Label>Endereço</Form.Label>
+                            <Form.Control
+                                type="text"
+                                defaultValue={baseForm.endereco}
+                                onChange={this.handleOnChange}
+                                placeholder="Rua ...."
+                                required/>
+                        </Form.Group>
+                    </Form.Row>
+
+                    <Form.Row>
+                        <Form.Group as={Col} controlId="cpf">
+                            <Form.Label>CPF</Form.Label>
+                            <Form.Control defaultValue={baseForm.cpf} onChange={this.handleOnChange} required/>
+                        </Form.Group>
+                        <Form.Group as={Col} controlId="email">
+                            <Form.Label>Email</Form.Label>
+                            <Form.Control
+                                type="email"
+                                defaultValue={baseForm.email}
+                                onChange={this.handleOnChange}
+                                placeholder="Enter email"
+                                required/>
+                        </Form.Group>
+                    </Form.Row>
+
+                    <Form.Row>
+                        <Form.Group as={Col} controlId="telefone">
+                            <Form.Label>Telefone</Form.Label>
+                            <Form.Control
+                                placeholder="XX-XXXXX-XXXX"
+                                onChange={this.handleOnChange}
+                                defaultValue={baseForm.telefone}
+                                required/>
+                        </Form.Group>
+                    </Form.Row>
+
+                    <Form.Row>
+                        <Form.Group as={Col} controlId="dominios">
+                            <Form.Label>Dominios</Form.Label>
+                            <Form.Control
+                                type="text"
+                                onChange={this.handleOnChange}
+                                placeholder="Separados por Virgula"
+                                defaultValue={baseForm.dominios}
+                                required/>
+                        </Form.Group>
+                    </Form.Row>
+
+                    <Button variant="primary" type="submit">
+                        Enviar
+                    </Button>
+
+                </Form>
             </div>
         )
     }
